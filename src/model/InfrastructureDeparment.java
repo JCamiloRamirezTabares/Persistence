a825ffdc29569376b2f04561271f7db50e430231
Juan Camilo <juancaram21@gmail.com>
2021-09-11 22:48:54 -0500

    Se agrega la segunda funcion, ahora permite agregar vallas desde consola y actualiza el csv

--- src/model/InfrastructureDeparment.java
+++ src/model/InfrastructureDeparment.java
@@ -2,6 +2,7 @@ package model;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,26 +19,46 @@ public class InfrastructureDeparment {
 	//Constructor
 	public InfrastructureDeparment() throws IOException {
 		list = new ArrayList<Bildboard>();
-		br = new BufferedReader(new FileReader(BILDBOARD_FILE_NAME));
-		
-		String line = br.readLine();
-		line = br.readLine();
-		while(line != null) {
-			String parts[] = line.split(separatorCharacter);
-			
-			double width = Double.parseDouble(parts[0]);
-			double height = Double.parseDouble(parts[1]);
-			boolean inUse = Boolean.parseBoolean(parts[2]);
-			
-			list.add(new Bildboard(width, height, inUse, parts[3]));
-			line = br.readLine();
-		}
-		br.close();
+		importData();
 	}
 
 	public List<Bildboard> getList() {
 		return list;
 	}
 	
+	public void importData() throws IOException {
+		br = new BufferedReader(new FileReader(BILDBOARD_FILE_NAME));
+
+		String line = br.readLine();
+		line = br.readLine();
+		while(line != null) {
+			String parts[] = line.split(separatorCharacter);
+
+			double width = Double.parseDouble(parts[0]);
+			double height = Double.parseDouble(parts[1]);
+			boolean inUse = Boolean.parseBoolean(parts[2]);
+
+			list.add(new Bildboard(width, height, inUse, parts[3]));
+			line = br.readLine();
+		}
+		br.close();
+	}
 	
+	public boolean addBillboard(double w, double h, boolean inUse, String brand) throws IOException {
+		
+		Bildboard newBillboard = new Bildboard(w, h, inUse, brand);
+		FileWriter fw = new FileWriter(BILDBOARD_FILE_NAME, true);
+		fw.write(w + "|" + h + "|" + inUse + "|" + brand + "\n");
+		
+		fw.close();
+		
+		if(list.add(newBillboard)) {
+			return true;
+		} else {
+			return false;
+		}
+		
+		
+	}
+
 }
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -8,7 +8,7 @@ import model.InfrastructureDeparment;
 
 public class Main {
 	
-	private final int EXIT_OPTION = 5;
+	private final static int EXIT_OPTION = 5;
 	
 	private BufferedReader br;
 	
@@ -22,24 +22,31 @@ public class Main {
 	public static void main(String [] juank) throws IOException {
 		Main app = new Main();
 		int option = 0;
+		String line = "";
 		
 		System.out.println("|| WELCOME TO BILDBOARD DEPARMENT SYSTEM ||\n");
 		System.out.println("Please, choose the option that you wanna do:");
-		System.out.println("( 1 ) Allows add a new Billdboard at the System \n( 2 ) Print the current Billboards \n");
-		System.out.print("Option: ");
-		String line = app.br.readLine();
+		System.out.println("( 1 ) Allows add a new Billdboard at the System \n( 2 ) Print the current Billboards \n( 5 ) To exit");
 		
-		option = Integer.parseInt(line);
-		
-		switch(option) {
+		do {
+			System.out.print("Option: ");
+			line = app.br.readLine();
+			option = Integer.parseInt(line);
+			
+			switch(option) {
 			case 1:
-				System.out.println("Hola aun estoy vacio");
+				app.addNewBillboard();
 				break;
 			case 2:
 				app.printBildboards();
 				break;
+			case 5:
+				System.out.println("Bye :3, I will see you later");
+
+			}
 			
-		}
+		}while(option != EXIT_OPTION);
+		
 	}
 	
 	public void printBildboards() {
@@ -53,9 +60,36 @@ public class Main {
 			infoBilboards += " " + deparment.getList().get(i).isInUse() + "  ";
 			infoBilboards += " " + deparment.getList().get(i).getBrand() + "  ";
 			infoBilboards += "\n";
+			
 		}
 		
 		System.out.println(infoBilboards);
+		
+		System.out.println("Total: " + deparment.getList().size() + " Bildboards");
+		
+	}
+	
+	public void addNewBillboard() throws IOException {
+		System.out.println("\n===============================\n    Billboard Formulary    \n===============================\n");
+		System.out.println("Please fill the following fields about the new Billboard");
+		System.out.print("Weight: ");
+		String w = br.readLine();
+		System.out.print("Height: ");
+		String h = br.readLine();
+		System.out.print("Is in Use?: ");
+		String use = br.readLine();
+		System.out.print("What is the brand of the Billboard: ");
+		String brand = br.readLine();
+		
+		double weight = Double.parseDouble(w);
+		double height = Double.parseDouble(w);
+		boolean inUse = Boolean.parseBoolean(use);
+		
+		if(deparment.addBillboard(weight, height, inUse, brand)) {
+			System.out.println("The billboard has been added successfully");
+		} else {
+			System.out.println("An error occurred while registering the new Billboard");
+		}
 		
 	}
 }
